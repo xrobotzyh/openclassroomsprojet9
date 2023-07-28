1ec08c70fe7b66e2262b7867bac586e408fd7737
zhao yh <makai87kai@outlook.com>
2023-07-28 15:59:13 +0200

    site using images upload

--- LiteReview/admin.py
+++ LiteReview/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import Userinfo
+from .models import User
 
-admin.site.register(Userinfo, UserAdmin)
+admin.site.register(User, UserAdmin)
 
 # Register your models here.
